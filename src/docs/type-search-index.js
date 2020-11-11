ee44fc473d1cbdb9d6c90b6df65d0aa0d365c862
ViktorHagstrom <viktor.hagstrom@hotmail.se>
2020-11-11 14:22:02 +0100

    Moved docs to src

---
+++ src/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Dice"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Player"}];updateSearchResults();
